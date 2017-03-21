c37bcca42efddae3405e515b13d96b093370f644
Marcel Nogueira and Zach Bastian <marcel@>
2017-03-21 10:19:19 -0700

    it will add html, scripts, styles, jquery.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $(".Needle").click(function() {
+    $(".space").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".Market").click(function() {
+    $(".pike").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".TeenSpirit").click(function() {
+    $(".teen").toggle();
+  });
+});
