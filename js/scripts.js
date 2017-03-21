9a31bdf7c1d36895c04ade32d9bf5586a351c193
Marcel Nogueira and Zach Bastian <marcel@>
2017-03-21 11:52:21 -0700

    it will add buttons, and flavor to your life.

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,39 @@ $(document).ready(function() {
   $(".Needle").click(function() {
     $(".space").toggle();
   });
-});
 
-$(document).ready(function() {
   $(".Market").click(function() {
     $(".pike").toggle();
   });
-});
 
-$(document).ready(function() {
   $(".TeenSpirit").click(function() {
     $(".teen").toggle();
+  });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("black-background");
+    $("p").removeClass();
+    $("p").addClass("white-text");
+});
+
+$("button#yellow").click(function() {
+  $("body").removeClass();
+  $("body").addClass("gray-background");
+  $("p").removeClass();
+  $("p").addClass("purple-text");
+});
+
+$("button#red").click(function() {
+  $("body").removeClass();
+  $("body").addClass("blue-background");
+  $("p").removeClass();
+  $("p").addClass("pink-text");
+
+});
+
+  $("p.Lorem").click(function() {
+  $("p.Lorem").addClass("highlight");
+  $("p.Lorem").addClass("box");
   });
 });
